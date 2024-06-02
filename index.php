00de40a457bd8d8d840522988c4cbca5fd7d73cb
Aurio-Bruno <auriobrunoab@gmail.com>
2024-06-02 22:49:41 +0100

    Fiz a tela de Login

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rede Social Responsiva</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="fontawesome-free-6.5.2-web/css/all.min.css">
+    <link rel="stylesheet" href="css/estilo.css">
+
+</head>
+<body>
+    <div class="container text-center" style="row-gap: 0;"> 
+        <div class="row">      
+            <div class="col-6">      
+                <img src="imagens/login-img.jpg" alt="" style="height: 650px;">                   
+            </div>
+            <div class="col-6" id="direito" style="background-color: #4484ce;">
+                <div>
+                    <h2>SOGA</h2>
+                    <input type="text" class="form-control" id="studentNumber" placeholder="Nº de estudante">
+                    <input type="text" class="form-control" id="password" placeholder="Senha">
+                    <button class="btn btn-primary" id="btn-entrar">Entrar</button> 
+                    <button class="btn btn-outline-secondary">Catrastrar-se</button>                    
+                </div>                
+            </div>
+        </div>     
+     </div>
+
+        
+    
+
+     <script src="bootstrap-5.3.3/js/bootstrap.min.js"></script>
+   <script src="js/jquery.js"></script>-
+    <script src="fontawesome-free-6.5.2-web/js/all.min.js"></script>
+</body>
+</html>
